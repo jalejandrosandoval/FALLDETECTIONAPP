cfe93a571632713b963ee25aecd784d21f1b4deb
jalejandrosandoval <jsandoval5@udi.edu.co>
2023-11-18 12:56:20 -0500

    Se ajustan las rutas de imagenes.

--- lib/Components/login.dart
+++ lib/Components/login.dart
@@ -71,7 +71,7 @@ class _LoginMainState extends State<LoginMain> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Image.asset("assetsassets/images/login.png"),
+            Image.asset("assets/images/login.png"),
             Container(
               margin: const EdgeInsets.symmetric(horizontal: 30.0),
               child: TextField(
