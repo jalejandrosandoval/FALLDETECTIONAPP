12d66726ad97afc72ad22dae69ca0770cba3f9d4
faiber.losada <faiber.losada@logytechmobile.com>
2023-11-18 15:19:10 -0500

    se adiciona Scroll

--- lib/Pages/eventspage.dart
+++ lib/Pages/eventspage.dart
@@ -1,9 +1,8 @@
-import 'package:falldetectionapp/Components/navbar.dart';
+import 'package:flutter/material.dart';
 import 'package:firebase_database/firebase_database.dart';
 import 'package:firebase_database/ui/firebase_animated_list.dart';
-import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
-
+import 'package:falldetectionapp/Components/navbar.dart';
 dynamic userData;
 
 class EventsPage extends StatelessWidget {
@@ -51,54 +50,53 @@ class EventsPageState extends State<EventsPageMain> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding( 
-        padding: const EdgeInsets.all(20.0),
-        child: Column(
-          children: <Widget>[
-            TextField(
-              onChanged: (value) {
-                setState(() {
-                  userNameSearch = value;
-                });
-                _getSearch(value);
-              },
-              decoration: InputDecoration(
-                icon: IconButton(
-                  icon: const Icon(Icons.search_outlined),
-                  onPressed: () {
-                    _getSearch(userNameSearch);
-                  },
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            children: <Widget>[
+              TextField(
+                onChanged: (value) {
+                  setState(() {
+                    userNameSearch = value;
+                  });
+                  _getSearch(value);
+                },
+                decoration: InputDecoration(
+                  icon: IconButton(
+                    icon: const Icon(Icons.search_outlined),
+                    onPressed: () {
+                      _getSearch(userNameSearch);
+                    },
+                  ),
+                  labelText: 'Buscar...',
+                  hintText: 'Buscar...',
+                  iconColor: _colorForms,
                 ),
-                labelText: 'Buscar...',
-                hintText: 'Buscar...',
-                iconColor: _colorForms,
               ),
-            ),
-            const Padding(padding: EdgeInsets.only(bottom: 20.0)),
-            FirebaseAnimatedList(
-              query: databaseReference,
-              shrinkWrap: true,
-              itemBuilder: (context, snapshot, animation, index) {
-                Map contact = snapshot.value as Map;
-                contact['key'] = snapshot.key;
-                return GestureDetector(
-                  child: 
-                  Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Padding(
+              const Padding(padding: EdgeInsets.only(bottom: 20.0)),
+              SizedBox(
+                height: MediaQuery.of(context).size.height - 250, // Ajusta según sea necesario
+                child: FirebaseAnimatedList(
+                  query: databaseReference,
+                  shrinkWrap: true,
+                  itemBuilder: (context, snapshot, animation, index) {
+                    Map contact = snapshot.value as Map;
+                    contact['key'] = snapshot.key;
+                    return GestureDetector(
+                      child: Padding(
                         padding: const EdgeInsets.all(4.0),
                         child: Card(
                           child: ListTile(
                             onTap: () {
                               Navigator.push(
                                 context,
-                                MaterialPageRoute(builder: (context) { 
+                                MaterialPageRoute(builder: (context) {
                                   return NavBarComponent(
-                                    latIngParam: LatLng(contact['latitud'], contact['longitud']), 
-                                    userDataParam: userData
-                                  ); 
-                                })
+                                    latIngParam: LatLng(contact['latitud'], contact['longitud']),
+                                    userDataParam: userData,
+                                  );
+                                }),
                               );
                             },
                             shape: RoundedRectangleBorder(
@@ -118,44 +116,47 @@ class EventsPageState extends State<EventsPageMain> {
                                         TextButton(
                                           style: TextButton.styleFrom(
                                             textStyle: Theme.of(context).textTheme.labelLarge,
-                                          ), 
-                                          onPressed: () { Navigator.pop(context, false); }, 
-                                          child: const Text("Cancelar")
+                                          ),
+                                          onPressed: () {
+                                            Navigator.pop(context, false);
+                                          },
+                                          child: const Text("Cancelar"),
                                         ),
                                         TextButton(
                                           style: TextButton.styleFrom(
                                             textStyle: Theme.of(context).textTheme.labelLarge,
                                           ),
-                                          onPressed: () { databaseReference.child(contact['key']).remove().then((value) => Navigator.pop(context, false)); }, 
-                                          child: const Text("Ok")
+                                          onPressed: () {
+                                            databaseReference.child(contact['key']).remove().then((value) => Navigator.pop(context, false));
+                                          },
+                                          child: const Text("Ok"),
                                         ),
-                                      ]
+                                      ],
                                     );
                                   },
                                 );
                               },
                             ),
-                            title: 
-                            Text(
+                            title: Text(
                               contact['nombre'],
-                              style: const TextStyle(fontSize: 17, fontWeight: FontWeight.bold)
+                              style: const TextStyle(fontSize: 17, fontWeight: FontWeight.bold),
                             ),
                             subtitle: Text(
                               'Aceleración: ${contact['aceleracion']} m/s^2. \nUbicación: (Latitud: ${contact['latitud']}, Longitud: ${contact['longitud']}.)',
-                              style: const TextStyle(fontSize: 14)
+                              style: const TextStyle(fontSize: 14),
                             ),
-                            isThreeLine: true
-                          )
-                        )
-                      )
-                    ],
-                  ) 
-                );
-              }
-            )
-          ]
-        )
-      )
+                            isThreeLine: true,
+                          ),
+                        ),
+                      ),
+                    );
+                  },
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
